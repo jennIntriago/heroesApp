ac34fdb5ad7289e184c908c13eb6ea94f1d364ed
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-10-17 19:38:45 -0500

    pruebas unitarias en el authReducer

--- tests/auth/context/authReducer.test.js
+++ tests/auth/context/authReducer.test.js
@@ -1,4 +1,4 @@
-import { authReduce } from "../../../src/auth/context/authReducer";
+import { authReduce, types } from "../../../src/auth";
 
 describe("Pruebas en el authReducer", () => {
   test("Debe de retornar el estado por defecto", () => {
@@ -6,9 +6,32 @@ describe("Pruebas en el authReducer", () => {
     expect(state).toEqual({ logged: false });
   });
   test("debe de (login) llamar el login autenticar y establecer el user", () => {
-    // second;
+    // accion de login
+    const action = {
+      type: types.login,
+      payload: {
+        name: "Jenn",
+        id: "123",
+      },
+    };
+    const state = authReduce({ logged: false }, action);
+    expect(state).toEqual({
+      logged: true,
+      user: action.payload,
+    });
   });
   test("debe de (logout) borrar el name del usuario y logged en false", () => {
-    // second;
+    //
+    const state = {
+      logged: true,
+      user: { id: "123", name: "Jenn" },
+    };
+
+    const action = {
+      type: types.logout,
+    };
+    const newState = authReduce(state, action);
+
+    expect(newState).toEqual({ logged: false });
   });
 });
